9fd48a2b91633cb443ab3e6d39777c59f6b4d419
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2026-05-16 16:00:33 +0530

    Refactor server.c for improved frame handling

--- GO-BACK-N ARQ /server.c
+++ GO-BACK-N ARQ /server.c
@@ -3,110 +3,31 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-
-#define MAX 80
-#define PORT 8080
-
-void func(int connfd)
-{
-    char buff[MAX];
-
-    int f, c, ack, next = 0;
-
-    while(1)
-    {
-        sleep(1);
-
-        bzero(buff, MAX);
-
-        recv(connfd, buff, MAX, 0);
-
-        // Exit condition
-        if(strcmp("Exit", buff) == 0)
-        {
-            printf("Exit\n");
-            break;
-        }
-
-        f = atoi(buff);
-
-        // Discard out of order frames
-        if(f != next)
-        {
-            printf("Frame %d discarded\n", f);
-
-            printf("Acknowledgement sent: %d\n", ack);
-
-            bzero(buff, MAX);
-
-            snprintf(buff, sizeof(buff), "%d", ack);
-
-            send(connfd, buff, sizeof(buff), 0);
-
-            continue;
-        }
-
-        // Random condition
-        c = rand() % 3;
-
-        switch(c)
-        {
-            case 0:
-
-                // Frame lost
-                break;
-
-            case 1:
-
-                ack = f;
-
-                sleep(2);
-
-                printf("Frame %d received\n", f);
-
-                printf("Acknowledgement sent: %d\n", ack);
-
-                bzero(buff, MAX);
-
-                snprintf(buff, sizeof(buff), "%d", ack);
-
-                send(connfd, buff, sizeof(buff), 0);
-
-                next = ack + 1;
-
-                break;
-
-            case 2:
-
-                ack = f;
-
-                printf("Frame %d received\n", f);
-
-                printf("Acknowledgement sent: %d\n", ack);
-
-                bzero(buff, MAX);
-
-                snprintf(buff, sizeof(buff), "%d", ack);
-
-                send(connfd, buff, sizeof(buff), 0);
-
-                next = ack + 1;
-
-                break;
-        }
-    }
-}
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main()
 {
-    int sockfd, connfd, len;
+    int sockfd, newsockfd;
 
-    struct sockaddr_in servaddr, cli;
+    struct sockaddr_in server, client;
 
-    // Create socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int clientlen = sizeof(client);
 
-    if(sockfd == -1)
+    char buffer[100];
+
+    int frame;
+    int ack;
+    int nextframe = 0;
+
+    int randomvalue;
+
+    // Create TCP socket
+    sockfd = socket(AF_INET,
+                    SOCK_STREAM,
+                    0);
+
+    if (sockfd < 0)
     {
         printf("Socket creation failed\n");
         return 0;
@@ -115,49 +36,116 @@ int main()
     printf("Socket successfully created\n");
 
     // Server details
-    bzero(&servaddr, sizeof(servaddr));
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(PORT);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8080);
+    server.sin_addr.s_addr = INADDR_ANY;
 
     // Bind socket
-    if(bind(sockfd,
-       (struct sockaddr *)&servaddr,
-       sizeof(servaddr)) != 0)
+    if (bind(sockfd,
+             (struct sockaddr *)&server,
+             sizeof(server)) < 0)
     {
-        printf("Socket bind failed\n");
+        printf("Binding failed\n");
         return 0;
     }
 
     printf("Socket successfully binded\n");
 
     // Listen for client
-    if(listen(sockfd, 5) != 0)
-    {
-        printf("Listen failed\n");
-        return 0;
-    }
+    listen(sockfd, 5);
 
     printf("Server listening\n");
 
-    len = sizeof(cli);
+    // Accept client
+    newsockfd = accept(sockfd,
+                       (struct sockaddr *)&client,
+                       (socklen_t *)&clientlen);
 
-    // Accept connection
-    connfd = accept(sockfd,
-             (struct sockaddr *)&cli,
-             &len);
-
-    if(connfd < 0)
+    if (newsockfd < 0)
     {
-        printf("Server accept failed\n");
+        printf("Accept failed\n");
         return 0;
     }
 
     printf("Server accepted the client\n");
 
-    func(connfd);
+    // Receive frames continuously
+    while (1)
+    {
+        sleep(1);
 
+        memset(buffer, 0, sizeof(buffer));
+
+        recv(newsockfd,
+             buffer,
+             sizeof(buffer),
+             0);
+
+        // Exit condition
+        if (strcmp(buffer, "Exit") == 0)
+        {
+            printf("Exit\n");
+            break;
+        }
+
+        frame = atoi(buffer);
+
+        // Discard out-of-order frames
+        if (frame != nextframe)
+        {
+            printf("Frame %d discarded\n",
+                   frame);
+
+            printf("Acknowledgement sent: %d\n",
+                   ack);
+
+            sprintf(buffer, "%d", ack);
+
+            send(newsockfd,
+                 buffer,
+                 sizeof(buffer),
+                 0);
+
+            continue;
+        }
+
+        // Random frame loss simulation
+        randomvalue = rand() % 3;
+
+        switch (randomvalue)
+        {
+            case 0:
+
+                // Frame lost
+                break;
+
+            case 1:
+
+            case 2:
+
+                ack = frame;
+
+                printf("Frame %d received\n",
+                       frame);
+
+                printf("Acknowledgement sent: %d\n",
+                       ack);
+
+                sprintf(buffer, "%d", ack);
+
+                send(newsockfd,
+                     buffer,
+                     sizeof(buffer),
+                     0);
+
+                nextframe = ack + 1;
+
+                break;
+        }
+    }
+
+    // Close sockets
+    close(newsockfd);
     close(sockfd);
 
     return 0;
