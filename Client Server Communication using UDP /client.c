5da2db1a04f3cbcb6856b0125a5ad6d9ce8f69fc
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2026-05-16 17:49:18 +0530

    Update client.c

--- Client Server Communication using UDP /client.c
+++ Client Server Communication using UDP /client.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 
 int main()
 {
@@ -12,12 +10,10 @@ int main()
 
     struct sockaddr_in server;
 
-    int serverlen = sizeof(server);
+    int len = sizeof(server);
 
-    int a[2][2], b[2][2];
-    int flat[100];
-
-    int i, j, k;
+    char str[100];
+    char result[100];
 
     // Create UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -32,97 +28,32 @@ int main()
     server.sin_family = AF_INET;
     server.sin_port = htons(8090);
 
-    inet_pton(AF_INET, "127.0.0.1",
+    inet_pton(AF_INET,
+              "127.0.0.1",
               &server.sin_addr);
 
-    // Input first matrix
-    printf("Enter elements for 1st matrix:\n");
+    // Input string
+    printf("Enter string: ");
+    scanf("%s", str);
 
-    for (i = 0; i < 2; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            printf("[%d][%d]: ",
-                   i + 1, j + 1);
-
-            scanf("%d", &a[i][j]);
-        }
-    }
-
-    // Convert first matrix to array
-    k = 0;
-
-    for (i = 0; i < 2; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            flat[k++] = a[i][j];
-        }
-    }
-
-    // Send first matrix
+    // Send string to server
     sendto(sockfd,
-           flat,
-           sizeof(flat),
+           str,
+           sizeof(str),
            0,
            (struct sockaddr *)&server,
-           serverlen);
+           len);
 
-    // Input second matrix
-    printf("\nEnter elements for 2nd matrix:\n");
-
-    for (i = 0; i < 2; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            printf("[%d][%d]: ",
-                   i + 1, j + 1);
-
-            scanf("%d", &b[i][j]);
-        }
-    }
-
-    // Convert second matrix to array
-    k = 0;
-
-    for (i = 0; i < 2; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            flat[k++] = b[i][j];
-        }
-    }
-
-    // Send second matrix
-    sendto(sockfd,
-           flat,
-           sizeof(flat),
-           0,
-           (struct sockaddr *)&server,
-           serverlen);
-
-    // Receive result matrix
+    // Receive result from server
     recvfrom(sockfd,
-             flat,
-             sizeof(flat),
+             result,
+             sizeof(result),
              0,
              (struct sockaddr *)&server,
-             &serverlen);
+             &len);
 
     // Display result
-    printf("\nResult matrix received from server:\n");
-
-    k = 0;
-
-    for (i = 0; i < 2; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            printf("%d\t", flat[k++]);
-        }
-
-        printf("\n");
-    }
+    printf("Server says: %s\n", result);
 
     // Close socket
     close(sockfd);
