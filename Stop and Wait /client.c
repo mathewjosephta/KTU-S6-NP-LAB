8c3b3468c7e2965d158dafa53523349dae74338a
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2026-05-16 15:52:33 +0530

    Refactor client.c for socket handling and frame sending

--- Stop and Wait /client.c
+++ Stop and Wait /client.c
@@ -3,25 +3,26 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-
-#define PORT 8080
-#define SIZE 1024
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main()
 {
-    int sock;
+    int sockfd;
 
     struct sockaddr_in server;
 
-    char buffer[SIZE];
+    char buffer[1024];
 
     int frame = 0;
-    int total_frames;
+    int totalframes;
 
-    // Create socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    int readval;
 
-    if (sock < 0)
+    // Create TCP socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0)
     {
         printf("Socket creation failed\n");
         return 0;
@@ -29,35 +30,47 @@ int main()
 
     // Server details
     server.sin_family = AF_INET;
-    server.sin_port = htons(PORT);
+    server.sin_port = htons(8080);
 
-    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
+    inet_pton(AF_INET,
+              "127.0.0.1",
+              &server.sin_addr);
 
     // Connect to server
-    connect(sock, (struct sockaddr *)&server, sizeof(server));
+    connect(sockfd,
+            (struct sockaddr *)&server,
+            sizeof(server));
 
-    // Input number of frames
+    // Input total frames
     printf("Enter the total number of frames to send: ");
-    scanf("%d", &total_frames);
+    scanf("%d", &totalframes);
 
     // Send frames one by one
-    while (frame < total_frames)
+    while (frame < totalframes)
     {
-        // Send frame
+        // Convert frame number to string
         sprintf(buffer, "%d", frame);
 
-        send(sock, buffer, strlen(buffer), 0);
+        // Send frame
+        send(sockfd,
+             buffer,
+             strlen(buffer),
+             0);
 
         printf("Client: Sent frame %d\n", frame);
 
+        // Clear buffer
+        memset(buffer, 0, sizeof(buffer));
+
         // Receive acknowledgment
-        memset(buffer, 0, SIZE);
+        readval = read(sockfd,
+                       buffer,
+                       sizeof(buffer));
 
-        int valread = read(sock, buffer, SIZE);
-
-        if (valread > 0)
+        if (readval > 0)
         {
-            printf("Client: Received acknowledgment: %s\n", buffer);
+            printf("Client: Received acknowledgment: %s\n",
+                   buffer);
 
             frame++;
         }
@@ -69,7 +82,8 @@ int main()
 
     printf("All frames sent successfully\n");
 
-    close(sock);
+    // Close socket
+    close(sockfd);
 
     return 0;
 }
