a3dbd06ece16d1cda4b93e97cb8692bb3a7efb9f
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2026-05-18 13:25:20 +0530

    Refactor client.c for clarity and improvements
    Refactored client code for better readability and structure. Increased filename buffer size and improved socket connection messages.

--- File Transfer Protocol /client.c
+++ File Transfer Protocol /client.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <stdlib.h>
 
 int main()
 {
+    int clientsock;
+    int n;
+
     struct sockaddr_in serveraddr;
 
-    int clientsock;
-    int n, length;
-
-    char filename[20];
+    char filename[100];
     char filedata[300];
 
-    // Clear structure
-    bzero((char *)&serveraddr, sizeof(serveraddr));
-
-    // Server details
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(8086);
-    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    // Create socket
+    // Create TCP socket
     clientsock = socket(AF_INET, SOCK_STREAM, 0);
 
     if (clientsock < 0)
@@ -33,36 +25,53 @@ int main()
         return 0;
     }
 
+    // Initialize structure
+    memset(&serveraddr, 0, sizeof(serveraddr));
+
+    // Server details
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(8086);
+
+    inet_pton(AF_INET,
+              "127.0.0.1",
+              &serveraddr.sin_addr);
+
     // Connect to server
     if (connect(clientsock,
-        (struct sockaddr *)&serveraddr,
-        sizeof(serveraddr)) < 0)
+                (struct sockaddr *)&serveraddr,
+                sizeof(serveraddr)) < 0)
     {
         printf("Connection failed\n");
         return 0;
     }
 
-    printf("Socket created successfully\n");
+    printf("Socket connected successfully\n");
 
     // Enter filename
-    printf("Enter the name of the file : ");
+    printf("Enter filename: ");
     scanf("%s", filename);
 
     // Send filename
-    length = strlen(filename);
-
-    write(clientsock, filename, length);
+    write(clientsock,
+          filename,
+          strlen(filename) + 1);
 
     // Receive file contents
-    n = read(clientsock, filedata, sizeof(filedata));
+    n = read(clientsock,
+             filedata,
+             sizeof(filedata));
 
-    filedata[n] = '\0';
+    if (n >= 0 && n < sizeof(filedata))
+    {
+        filedata[n] = '\0';
+    }
 
     // Display file contents
-    printf("\nThe contents of the file:\n");
+    printf("\nFile contents:\n");
 
     printf("%s\n", filedata);
 
+    // Close socket
     close(clientsock);
 
     return 0;
