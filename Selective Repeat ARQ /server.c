f5c1c4395a24d15a57447f59085b68ce41183630
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2026-05-19 16:27:37 +0530

    Update server.c

--- Selective Repeat ARQ /server.c
+++ Selective Repeat ARQ /server.c
@@ -9,36 +9,21 @@
 
 int main()
 {
-    int sockfd;
-
+    int sockfd, newsockfd, readval;
     struct sockaddr_in server, client;
-
     socklen_t clientlen = sizeof(client);
+    char str[100];
+    int i,frame,lostframe,windowsize = 5,ack[5] = {0};
 
-    char buffer[100];
-
-    int frame;
-    int lostframe;
-
-    int windowsize = 5;
-
-    int i;
-    int readval;
-
-    // Create UDP socket
-    sockfd = socket(AF_INET,
-                    SOCK_DGRAM,
-                    0);
-
+    // Create TCP socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
         printf("Socket creation failed\n");
         return 0;
     }
 
-    printf("Socket successfully created\n");
-
-    // Initialize structure
+    // Initialize server structure
     memset(&server, 0, sizeof(server));
 
     // Server details
@@ -47,9 +32,7 @@ int main()
     server.sin_addr.s_addr = INADDR_ANY;
 
     // Bind socket
-    if (bind(sockfd,
-             (struct sockaddr *)&server,
-             sizeof(server)) < 0)
+    if (bind(sockfd,(struct sockaddr *)&server,sizeof(server)) < 0)
     {
         printf("Binding failed\n");
         return 0;
@@ -57,92 +40,102 @@ int main()
 
     printf("Binding successful\n");
 
-    printf("Waiting for frames...\n");
+    // Listen for client
+    if (listen(sockfd, 3) < 0)
+    {
+        printf("Listening failed\n");
+        return 0;
+    }
+
+    printf("Waiting for connection...\n");
+
+    // Accept client
+    newsockfd = accept(sockfd,(struct sockaddr *)&client,&clientlen);
+
+    if (newsockfd < 0)
+    {
+        printf("Accept failed\n");
+        return 0;
+    }
+
+    printf("Connection established\n");
 
     srand(time(0));
 
     // Random lost frame
     lostframe = rand() % windowsize;
 
+    printf("Frame %d will be lost\n",
+           lostframe);
+
+    // Receive frames
     for (i = 0; i < windowsize; i++)
     {
-        memset(buffer, 0, sizeof(buffer));
+        memset(str, 0, sizeof(str));
 
         // Receive frame
-        readval = recvfrom(sockfd,
-                           buffer,
-                           sizeof(buffer),
-                           0,
-                           (struct sockaddr *)&client,
-                           &clientlen);
+        readval = read(newsockfd, str,sizeof(str));
 
-        buffer[readval] = '\0';
+        if (readval <= 0)
+        {
+            printf("Connection closed\n");
+            break;
+        }
 
-        frame = atoi(buffer);
+        str[readval] = '\0';
+
+        frame = atoi(str);
 
         // Simulate frame loss
-        if (frame == lostframe)
+        if (frame == lostframe &&
+            ack[frame] == 0)
         {
             printf("Frame %d lost\n",
                    frame);
 
+            ack[frame] = -1;
+
             continue;
         }
 
-        printf("Received frame %d\n",
-               frame);
+        printf("Frame %d received\n", frame);
 
         // Send ACK
-        sprintf(buffer,
-                "%d",
-                frame);
+        sprintf(str,"%d",frame);
 
-        sendto(sockfd,
-               buffer,
-               strlen(buffer) + 1,
-               0,
-               (struct sockaddr *)&client,
-               clientlen);
+        send(newsockfd,str,strlen(str) + 1,0);
 
         printf("Acknowledgment sent for frame %d\n",
                frame);
+
+        ack[frame] = 1;
     }
 
-    printf("\nWaiting for retransmitted frames...\n");
+    printf("\nWaiting for retransmitted frame...\n");
 
-    // Receive retransmitted frames
-    for (i = lostframe; i < windowsize; i++)
+    // Receive retransmitted frame
+    memset(str, 0, sizeof(str));
+
+    readval = read(newsockfd,str,sizeof(str));
+
+    if (readval > 0)
     {
-        memset(buffer, 0, sizeof(buffer));
+        str[readval] = '\0';
 
-        readval = recvfrom(sockfd,
-                           buffer,
-                           sizeof(buffer),
-                           0,
-                           (struct sockaddr *)&client,
-                           &clientlen);
+        frame = atoi(str);
 
-        buffer[readval] = '\0';
-
-        frame = atoi(buffer);
-
-        printf("Received retransmitted frame %d\n",
+        printf("Retransmitted frame %d received\n",
                frame);
 
         // Send ACK
-        sprintf(buffer,
-                "%d",
-                frame);
+        sprintf(str,"%d",frame);
 
-        sendto(sockfd,
-               buffer,
-               strlen(buffer) + 1,
-               0,
-               (struct sockaddr *)&client,
-               clientlen);
+        send(newsockfd,str,strlen(str) + 1,0);
+        printf("Acknowledgment sent for frame %d\n",frame);
     }
 
+    // Close sockets
+    close(newsockfd);
     close(sockfd);
-
     return 0;
 }
