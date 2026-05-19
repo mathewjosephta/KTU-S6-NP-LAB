f7ac71b9957fd7ed857bc46c299ef707f8c154ce
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2026-05-19 14:46:40 +0530

    Update client.c

--- Client Server Communication Using TCP /client.c
+++ Client Server Communication Using TCP /client.c
@@ -1,57 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
 
 int main()
 {
-    int sockfd,readval;
-    struct sockaddr_in server;
-    char str[100];
+int sockfd,readval;
+struct sockaddr_in server;
+char str[100];
 
-    // Create TCP socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        printf("Socket creation failed\n");
-        return 0;
-    }
+sockfd=socket(AF_INET,SOCK_STREAM,0);
 
-    // Initialize server structure
-    memset(&server, 0, sizeof(server));
+if(sockfd<0)
+{
+printf("Socket creation failed\n");
+return 0;
+}
 
-    // Server details
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8086);
-    inet_pton(AF_INET,"127.0.0.1",&server.sin_addr);
+memset(&server,0,sizeof(server));
 
-    // Connect to server
-    if (connect(sockfd,(struct sockaddr *)&server,sizeof(server)) < 0)
-    {
-        printf("Connection failed\n");
-        return 0;
-    }
+server.sin_family=AF_INET;
+server.sin_port=htons(8086);
 
-    // Input string
-    printf("Enter string: ");
-    scanf("%s", str);
+inet_pton(AF_INET,"127.0.0.1",&server.sin_addr);
 
-    // Send string to server
-    send(sockfd,str,strlen(str) + 1,0);
+if(connect(sockfd,(struct sockaddr *)&server,sizeof(server))<0)
+{
+printf("Connection failed\n");
+return 0;
+}
 
-    // Receive reversed string
-    readval = read(sockfd,str,sizeof(str));
+printf("Connected\n");
 
-    str[readval] = '\0';
+printf("Enter string: ");
 
-    // Display result
-    printf("Server says: %s\n", str);
+scanf("%s",str);
 
-    // Close socket
-    close(sockfd);
+send(sockfd,str,strlen(str)+1,0);
 
-    return 0;
+readval=read(sockfd,str,sizeof(str));
+
+if(readval>0)
+{
+str[readval]='\0';
+}
+
+printf("Server says: %s\n",str);
+
+close(sockfd);
+
+return 0;
 }
